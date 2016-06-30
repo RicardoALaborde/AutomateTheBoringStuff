efd3a709a0c14ee30c0fc75a9a36bf848d60ba6f
ricardolaborde <raltandi@Frankenstein-Station.snow>
2016-06-30 00:08:10 -0400

    Added comments to Character Picture Grid explaining the logic behind the challenge

--- Chapter4/CharacterPictureGrid.py
+++ Chapter4/CharacterPictureGrid.py
@@ -11,8 +11,10 @@ grid = [['.', '.', '.', '.', '.', '.'],
         ['.', 'O', 'O', '.', '.', '.'],
         ['.', '.', '.', '.', '.', '.']]
 
-
+#get length of first list, as it will determine the length of every other list, the lenght would act as a guide for X coordinates
 for a in range(len(grid[0])):
+	#get length of the list of lists, that is, the Y coordinates
 	for b in range(len(grid)):
+		#print out grid[0][0] then [1][0] then [2][0], and so on
 		print(grid[b][a],end='')
 	print(end='\n')
