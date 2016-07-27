9d61acc013976da292379486e77e3203a7a169b2
Ricardo Laborde Torres <ricardo.a.laborde@gmail.com>
2016-07-27 13:28:05 -0400

    Fixed downloading of wrong images
    Previous code was downloading images that had nothing to do with search query

--- AutomateTheBoringStuff/Chapter11/imageSiteDownloader.py
+++ AutomateTheBoringStuff/Chapter11/imageSiteDownloader.py
@@ -1,18 +1,14 @@
 '''
-Title: Image Site Downloader
-
 Write a program that goes to a photo-sharing site like Flickr or Imgur,
 searches for a category of photos, and then downloads all the resulting
 images. You could write a program that works with any photo site that
 has a search feature.
-
-Author: Ricardo Laborde
 '''
 
 import requests, bs4,sys,os
 
 #start at imgur search url
-url = 'http://imgur.com/search?q='+sys.argv[1]
+url = 'https://imgur.com/search?q='+sys.argv[1]
 #store the images in ./imgurImages
 os.makedirs('imgurImages', exist_ok=True)
 #counter for how many images are going to be downloaded
@@ -21,16 +17,19 @@ print('Downloading images from %s...' % url)
 res = requests.get(url)
 res.raise_for_status()
 soup = bs4.BeautifulSoup(res.text,'html.parser')
+#select any element with links to images
+imageElem = soup.select('a > img')
 
-#TODO: find correct selector for image results
-#i'm only downloading 5 of these because of space
+#i'm only downloading 5 of these because of space or the lenght of the image search result
 while imageCount!=5:
-    #select any element with a class name of 'overlay'
-    imageElem = soup.select('img')
+    if len(imageElem) == imageCount:
+        break
     imageUrl = 'https:' + imageElem[imageCount].get('src')
     print('Downloading from %s'%(imageUrl))
     res = requests.get(imageUrl)
+    #check if it exists
     res.raise_for_status()
+    #write contents of the resulting image url
     imageFile = open(os.path.join('imgurImages', os.path.basename(imageUrl)), 'wb')
     for chunk in res.iter_content(1000000):
         imageFile.write(chunk)
