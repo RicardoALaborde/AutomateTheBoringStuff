d422da72c8efa68b592891f40cbd3d008d05f97b
ricardo <ricardo.a.laborde@gmail.com>
2016-08-09 22:11:14 -0400

    Added multiplication table challenge
    Takes any argument and makes multiplication table from it

---
+++ AutomateTheBoringStuff/Chapter12/multiplicationTableMaker.py
@@ -0,0 +1,42 @@
+'''
+Create a program multiplicationTable.py that takes a number N from the command line
+and creates an N×N multiplication table in an Excel spreadsheet. For example, when
+the program is run like this:
+
+py multiplicationTable.py 6
+'''
+import openpyxl,sys
+from openpyxl.cell import get_column_letter, column_index_from_string
+
+try:
+    #validate we're getting an integer from the given arguments
+    int(sys.argv[1])
+except:
+    exit
+
+workBook=openpyxl.Workbook()
+sheet=workBook.active
+sheet.title = str(sys.argv[1]) + ' Multiplication Table'
+multiply = 1
+lstA = []
+for i in range(int(sys.argv[1])):
+    multiply +=1
+    #columns
+    sheet[get_column_letter(multiply)+'1']=multiply-1
+    lstA.append(multiply-1)
+    #rows
+    sheet['A'+str(multiply)]=multiply-1
+
+letter = 2
+multiplyIndex=0
+matrixLen = int(sys.argv[1])
+
+for i in range(matrixLen):
+    row=2
+    for m in range(matrixLen):
+        cell = get_column_letter(letter)+str(row)
+        sheet[cell]=lstA[i]*lstA[multiplyIndex+m]
+        row+=1
+    letter+=1
+
+workBook.save('MultiplicationTable.xlsx')
